1bf33d2c76b8638b1a7c619890c60e09511ed12e
srs-1123 <srs.23p2.1123@gmail.com>
2026-03-29 15:02:29 +0900

    fix: 参考書通りのソースコードに修正

--- src/statement.js
+++ src/statement.js
@@ -1,6 +1,11 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
+  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
+
+  const format = new Intl.NumberFormat("en-US",
+    { style: "currency", currency: "USD",
+      minimumFractionDigits: 2}).format;
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
@@ -9,21 +14,30 @@ function statement(invoice, plays) {
     switch (play.type) {
       case "tragedy":
         thisAmount = 40000;
-        if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30);
+        if (perf.audience > 30) 
+          thisAmount += 1000 * (perf.audience - 30);
         break;
       case "comedy":
         thisAmount = 30000;
-        if (perf.audience > 20) thisAmount += 500 * perf.audience + 10000;
+        if (perf.audience > 20) 
+          thisAmount += 500 * perf.audience + 10000;
         else thisAmount += 300 * perf.audience;
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
+
+    // ボリューム特典の時のポイントを加算
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 喜劇の時は10人につき、さらにポイントを加算
+    if ("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
+    // 注文の内訳を出力
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
-    result += ` ${play.name}: ${thisAmount / 100} (${perf.audience} seats)\n`;
   }
 
-  result += `Amount owed is ${totalAmount / 100}\n`;
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
 

fabec00fb3249ea248a9d600cbe0b8f907ef35dd
srs-1123 <srs.23p2.1123@gmail.com>
2026-03-29 15:17:05 +0900

    refactor: playを返す関数作成(問い合わせによる一時変数の置き換え)

--- src/statement.js
+++ src/statement.js
@@ -8,7 +8,7 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2}).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // ボリューム特典の時のポイントを加算
@@ -23,6 +23,11 @@ function statement(invoice, plays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  // 問い合わせによる一時変数の置き換え
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
 
   // 関数の抽出
   function amountFor(aPerformance, play) {

5b03c1183a6ad9df9aa2b86e634736d8c73ab14e
srs-1123 <srs.23p2.1123@gmail.com>
2026-03-31 23:40:27 +0900

    performancesを中間オブジェクト経由で渡す形に変更

--- src/statement.js
+++ src/statement.js
@@ -1,13 +1,14 @@
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
   let result = `Statement for ${data.customer}\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     // 注文の内訳を出力
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
   }

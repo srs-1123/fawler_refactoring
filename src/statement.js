af4bda295816a7b2e5de65272f043cabacc2f093
srs-1123 <srs.23p2.1123@gmail.com>
2026-04-04 01:16:57 +0900

    不要な引数を削除

--- src/statement.js
+++ src/statement.js
@@ -4,7 +4,7 @@ function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
 
   for (let perf of data.performances) {

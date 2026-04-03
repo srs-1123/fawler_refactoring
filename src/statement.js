280b6b6a805c2f3491f7734cffe07320ebf3c7a3
srs-1123 <srs.23p2.1123@gmail.com>
2026-04-04 01:04:24 +0900

    パイプラインによるループの置き換え

--- src/statement.js
+++ src/statement.js
@@ -51,19 +51,13 @@ function statement(invoice, plays) {
 
     // 関数の抽出
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances
+      .reduce((total, p) => total + p.amount, 0);
   }
 }
 

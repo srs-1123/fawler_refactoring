7bd1ac2f286a8dcd325b3f09d4cf2e281f32bebe
srs-1123 <srs.23p2.1123@gmail.com>
2026-04-04 18:43:27 +0900

    amountFor関数のインライン化

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -10,7 +10,7 @@ function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
+    result.amount = calculator.amount;
     result.volumeCredits = volumeCreditsFor(result);
     return result;
   }

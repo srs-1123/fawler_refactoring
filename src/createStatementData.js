a881d14dbb0c6812072f0cfd2ae429c7d8e9eba2
srs-1123 <srs.23p2.1123@gmail.com>
2026-04-04 18:46:17 +0900

    volumeCredits関数をPerformanceCalculatorに移動

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -27,10 +27,7 @@ function createStatementData(invoice, plays) {
   // 関数の抽出
   // 余計なコメントアウトは関数の抽出に伴い削除
   function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" == aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
-    return volumeCredits;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
   }
 
     // 関数の抽出
@@ -69,6 +66,13 @@ class PerformanceCalculator {
       default:
         throw new Error(`unknown type: ${this.play.type}`);
     }
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" == this.performance.play.type) result += Math.floor(this.performance.audience / 5);
     return result;
   }
 }

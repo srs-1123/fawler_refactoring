5f40e45f73469fbff2e9ddc2f4c649d84e818b37
srs-1123 <srs.23p2.1123@gmail.com>
2026-04-04 18:39:23 +0900

    amount関数の移動

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -67,6 +67,27 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = aPlay;
   }
+
+  get amount() {
+    let result = 0; // 関数名で何を返すのかは伝わるので、結果はresultという変数名にする
+    // 変数のインライン化(playFor()を直接使う)
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) 
+          result += 1000 * (this.performance.audience - 30);
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) 
+          result += 500 * this.performance.audience + 10000;
+        else result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
+  }
 }
 
 module.exports = { createStatementData };

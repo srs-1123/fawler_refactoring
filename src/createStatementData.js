2617ab987cd7cd8ba2ee424176cae8f1ea1a20be
srs-1123 <srs.23p2.1123@gmail.com>
2026-04-04 18:51:36 +0900

    関数によるコンストラクタの置き換え

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -7,7 +7,7 @@ function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const calculator = CreatePerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
@@ -64,6 +64,10 @@ class PerformanceCalculator {
     if ("comedy" == this.play.type) result += Math.floor(this.performance.audience / 5);
     return result;
   }
+}
+
+function CreatePerformanceCalculator(aPerformance, aPlay) {
+  return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 module.exports = { createStatementData };

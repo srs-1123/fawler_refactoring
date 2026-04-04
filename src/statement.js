467e2fe5ec06c580a08749b3b1cdab2174709978
srs-1123 <srs.23p2.1123@gmail.com>
2026-04-04 12:40:05 +0900

    htmlStatementを追加

--- src/statement.js
+++ src/statement.js
@@ -15,14 +15,37 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-
-  // 関数の抽出 & 関数宣言の変更
-  // 元のformat()だと何をしているか分からなかったためusd()に変更
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US",
-                        { style: "currency", currency: "USD",
-                          minimumFractionDigits: 2}).format(aNumber);
-  }
 }
 
-module.exports = { statement };
+// フェーズの分離により請求処理部分と請求書処理のフォーマット作成部分を分離したため
+// 別フォーマットの関数を簡単に作れるようになった
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+
+  return result;
+}
+
+// 関数の抽出 & 関数宣言の変更
+// 元のformat()だと何をしているか分からなかったためusd()に変更
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US",
+                      { style: "currency", currency: "USD",
+                        minimumFractionDigits: 2}).format(aNumber);
+}
+
+module.exports = { statement, htmlStatement };
